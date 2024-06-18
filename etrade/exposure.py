3249ec2cb2f15ce48734521917945c032ea97248
JackKirbyCook82 <jackcookchee@yahoo.com>
2024-06-18 16:23:29 -0500

    Updated 1/10/2024

--- etrade/exposure.py
+++ etrade/exposure.py
@@ -11,6 +11,7 @@ import sys
 import logging
 import warnings
 import numpy as np
+from datetime import datetime as Datetime
 
 MAIN = os.path.dirname(os.path.realpath(__file__))
 PROJECT = os.path.abspath(os.path.join(MAIN, os.pardir))
@@ -24,6 +25,7 @@ from finance.securities import SecurityCalculator, SecurityFilter, SecurityFiles
 from finance.exposures import ExposureCalculator, ExposureFiles
 from finance.variables import Variables, Strategies
 from finance.strategies import StrategyCalculator
+from finance.technicals import TechnicalFiles
 from finance.holdings import HoldingFiles
 from support.files import Loader, Saver, FileTypes, FileTimings
 from support.synchronize import SideThread
@@ -36,13 +38,13 @@ __copyright__ = "Copyright 2023, Jack Kirby Cook"
 __license__ = "MIT License"
 
 
-def exposure(*args, source, destination, parameters={}, **kwargs):
+def exposure(*args, source, destination, current, parameters={}, **kwargs):
     holding_loader = Loader(name="PortfolioHoldingLoader", source=source)
     exposure_calculator = ExposureCalculator(name="PortfolioExposureCalculator")
     exposure_saver = Saver(name="PortfolioExposureSaver", destination=destination)
     exposure_pipeline = holding_loader + exposure_calculator + exposure_saver
     exposure_thread = SideThread(exposure_pipeline, name="PortfolioExposureThread")
-    exposure_thread.setup(**parameters)
+    exposure_thread.setup(current=current, **parameters)
     return exposure_thread
 
 
@@ -75,14 +77,15 @@ def main(*args, **kwargs):
     criterion = dict(security=security_criterion, valuation=valuation_criterion)
     functions = dict(size=lambda cols: np.int32(10), volume=np.NaN, interest=np.NaN)
     calculations = [Strategies.Collar.Long, Strategies.Collar.Short, Strategies.Vertical.Put, Strategies.Vertical.Call]
+    statistic_file = TechnicalFiles.Statistic(name="StatisticFile", repository=PORTFOLIO, filetype=FileTypes.CSV, filetiming=FileTimings.EAGER)
     holdings_file = HoldingFiles.Holding(name="HoldingFile", repository=PORTFOLIO, filetype=FileTypes.CSV, filetiming=FileTimings.EAGER)
     exposure_file = ExposureFiles.Exposure(name="ExposureFile", repository=PORTFOLIO, filetype=FileTypes.CSV, filetiming=FileTimings.EAGER)
     option_file = SecurityFiles.Options(name="OptionFile", repository=PORTFOLIO, filetype=FileTypes.CSV, filetiming=FileTimings.EAGER)
     arbitrage_file = ValuationFiles.Arbitrage(name="ArbitrageFile", repository=PORTFOLIO, filetype=FileTypes.CSV, filetiming=FileTimings.EAGER)
     exposure_parameters = dict(source={holdings_file: "r"}, destination={exposure_file: "w"}, )
     exposure_thread = exposure(*args, **exposure_parameters, **kwargs)
-    security_parameters = dict(source={exposure_file: "r"}, destination={option_file: "w"}, functions=functions)
-    security_thread = exposure(*args, **security_parameters, **kwargs)
+    security_parameters = dict(source={exposure_file: "r", statistic_file: "r"}, destination={option_file: "w"}, functions=functions)
+    security_thread = security(*args, **security_parameters, **kwargs)
     valuation_parameters = dict(source={option_file: "r"}, destination={arbitrage_file: "w"}, calculations=calculations, criterion=criterion)
     valuation_thread = valuation(*args, **valuation_parameters, **kwargs)
     exposure_thread.start()
@@ -96,7 +99,7 @@ def main(*args, **kwargs):
 if __name__ == "__main__":
     logging.basicConfig(level="INFO", format="[%(levelname)s, %(threadName)s]:  %(message)s", handlers=[logging.StreamHandler(sys.stdout)])
     warnings.filterwarnings("ignore")
-    main(parameters={"discount": 0.0, "fees": 0.0})
+    main(current=Datetime(year=2024, month=6, day=17), parameters={"discount": 0.0, "fees": 0.0})
 
 
 
