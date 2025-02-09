cf91841c4d7eab4afcc6f5d5c356d925a42f3ceb
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-02-09 03:41:22 -0600

    Updated 1/10/2024

--- acquisition.py
+++ acquisition.py
@@ -18,11 +18,13 @@ ROOT = os.path.abspath(os.path.join(MAIN, os.pardir))
 REPOSITORY = os.path.join(ROOT, "repository")
 if ROOT not in sys.path: sys.path.append(ROOT)
 
-from finance.prospects import ProspectCalculator, ProspectWriter, ProspectTable, ProspectHeader, ProspectLayout
+from finance.prospects import ProspectCalculator, ProspectWriter, ProspectDiscarding, ProspectReader
+from finance.prospects import ProspectTable, ProspectHeader, ProspectLayout
 from finance.valuations import ValuationCalculator
 from finance.strategies import StrategyCalculator
-from finance.variables import Variables, Querys, Files
-from support.pipelines import Producer, Processor, Consumer
+from finance.securities import SecurityCalculator
+from finance.variables import Variables, Querys, Files, Strategies
+from support.pipelines import Routine, Producer, Processor, Consumer
 from support.synchronize import RoutineThread
 from support.files import Directory, Loader
 from support.transforms import Pivoter
@@ -38,42 +40,51 @@ __license__ = "MIT License"
 class OptionDirectory(Directory, Producer, query=Querys.Settlement): pass
 class OptionLoader(Loader, Processor, query=Querys.Settlement): pass
 class OptionFilter(Filter, Processor, query=Querys.Settlement): pass
+class SecurityCalculator(SecurityCalculator, Processor): pass
+class SecurityFilter(Filter, Processor, query=Querys.Settlement): pass
 class StrategyCalculator(StrategyCalculator, Processor): pass
 class ValuationCalculator(ValuationCalculator, Processor): pass
 class ValuationPivoter(Pivoter, Processor, query=Querys.Settlement): pass
 class ValuationFilter(Filter, Processor, query=Querys.Settlement): pass
 class ProspectCalculator(ProspectCalculator, Processor): pass
-class ProspectWriter(ProspectWriter, Consumer): pass
-class AcquisitionFile(Files.Options.Trade + Files.Options.Quote): pass
+class ProspectWriter(ProspectWriter, Consumer, query=Querys.Settlement): pass
+class ProspectRemover(ProspectDiscarding, Routine, query=Querys.Settlement): pass
+class ProspectReader(ProspectReader, Producer, query=Querys.Settlement): pass
+
+class AcquisitionFile(Files.Options.Trade + Files.Options.Quote):
+    pass
 
 
 def main(*args, criterion={}, discount, fees, **kwargs):
+    valuation_criterion = lambda table: (table[("apy", Variables.Valuations.Scenario.MINIMUM)] >= criterion["apy"]) & (table[("size", "")] >= criterion["size"])
+    security_criterion = lambda table: table["size"] >= criterion["size"]
+    acquisition_priority = lambda columns: columns[("apy", Variables.Valuations.Scenario.MINIMUM)]
+
     acquisition_file = AcquisitionFile(name="AcquisitionFile", folder="market", repository=REPOSITORY)
-    acquisition_layout = ProspectLayout(name="AcquisitionLayout", valuation=Variables.Valuations.ARBITRAGE, rows=100)
-    acquisition_header = ProspectHeader(name="AcquisitionHeader", valuation=Variables.Valuations.ARBITRAGE)
+    acquisition_layout = ProspectLayout(name="AcquisitionLayout", valuation=Variables.Valuations.Valuation.ARBITRAGE, rows=100)
+    acquisition_header = ProspectHeader(name="AcquisitionHeader", valuation=Variables.Valuations.Valuation.ARBITRAGE)
     acquisition_table = ProspectTable(name="AcquisitionTable", layout=acquisition_layout, header=acquisition_header)
-    acquisition_criterion = lambda table: table[("apy", Variables.Scenarios.MINIMUM)] >= criterion["apy"] & table[("size", Variables.Scenarios.MINIMUM)] >= criterion["size"]
-    acquisition_priority = lambda columns: columns[("apy", Variables.Scenarios.MINIMUM)]
-    acquisition_strategies = list(Variables.Strategies.Strategy)
-    acquisition_valuation = Variables.Valuations.Valuation.ARBITRAGE
 
     option_directory = OptionDirectory(name="OptionDirectory", file=acquisition_file, mode="r")
     option_loader = OptionLoader(name="OptionLoader", file=acquisition_file, mode="r")
-    strategy_calculator = StrategyCalculator(name="StrategyCalculator", strategies=acquisition_strategies)
-    valuation_calculator = ValuationCalculator(name="ValuationCalculator", valuation=acquisition_valuation)
+    security_calculator = SecurityCalculator(name="SecurityCalculator", pricing=Variables.Markets.Pricing.MARKET)
+    security_filter = SecurityFilter(name="SecurityFilter", criterion=security_criterion)
+    strategy_calculator = StrategyCalculator(name="StrategyCalculator", strategies=list(Strategies))
+    valuation_calculator = ValuationCalculator(name="ValuationCalculator", valuation=Variables.Valuations.Valuation.ARBITRAGE)
     valuation_pivoter = ValuationPivoter(name="ValuationPivoter", header=acquisition_header)
-    valuation_filter = ValuationFilter(name="ValuationFilter", criterion=acquisition_criterion)
+    valuation_filter = ValuationFilter(name="ValuationFilter", criterion=valuation_criterion)
     prospect_calculator = ProspectCalculator(name="ProspectCalculator", header=acquisition_header, priority=acquisition_priority)
     prospect_writer = ProspectWriter(name="ProspectWriter", table=acquisition_table)
-    acquisition_pipeline = option_directory + option_loader + strategy_calculator + valuation_calculator + valuation_pivoter + valuation_filter + prospect_calculator + prospect_writer
+    acquisition_pipeline = option_directory + option_loader + security_calculator + security_filter + strategy_calculator + valuation_calculator + valuation_pivoter + valuation_filter + prospect_calculator + prospect_writer
     acquisition_thread = RoutineThread(acquisition_pipeline, name="AcquisitionThread").setup(discount=discount, fees=fees)
 
     acquisition_thread.start()
     while bool(acquisition_thread):
-        if bool(acquisition_table): print(acquisition_table)
+        print(acquisition_table)
         time.sleep(10)
     acquisition_thread.cease()
     acquisition_thread.join()
+    print(acquisition_table)
 
 
 if __name__ == "__main__":
@@ -82,7 +93,7 @@ if __name__ == "__main__":
     pd.set_option("display.max_columns", 50)
     pd.set_option("display.max_rows", 50)
     pd.set_option("display.width", 250)
-    sysCriterion = dict(apy=2.00, size=10)
+    sysCriterion = dict(apy=0.05, size=10)
     main(criterion=sysCriterion, discount=0.00, fees=0.00)
 
 
