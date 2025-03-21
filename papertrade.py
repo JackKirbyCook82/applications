18c0054d65bf56aba085c7017a6f46c445391f25
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-03-20 23:24:40 -0500

    Updated 1/10/2024

--- papertrade.py
+++ papertrade.py
@@ -24,14 +24,14 @@ if ROOT not in sys.path: sys.path.append(ROOT)
 TICKERS = os.path.join(RESOURCES, "tickers.txt")
 API = os.path.join(RESOURCES, "api.txt")
 
-from alpaca.market import AlpacaStockDownloader, AlpacaOptionDownloader, AlpacaContractDownloader
+from alpaca.market import AlpacaOptionDownloader, AlpacaContractDownloader
 from alpaca.portfolio import AlpacaPortfolioDownloader
 from alpaca.orders import AlpacaOrderUploader
 from finance.market import AcquisitionCalculator, DivestitureCalculator
-from finance.prospects import ProspectCalculator
-from finance.valuations import ValuationCalculator
+from finance.securities import OptionCalculator
 from finance.strategies import StrategyCalculator
-from finance.securities import StockCalculator, OptionCalculator
+from finance.valuations import ValuationCalculator
+from finance.prospects import ProspectCalculator
 from finance.variables import Variables, Querys, Strategies
 from webscraping.webreaders import WebAuthorizerAPI, WebReader
 from support.pipelines import Producer, Processor, Consumer, Carryover
@@ -48,25 +48,21 @@ __license__ = "MIT License"
 
 
 class SymbolDequeuer(Dequeuer, Carryover, Producer, signature="->symbol"): pass
-class StockDownloader(AlpacaStockDownloader, Carryover, Processor, signature="symbol->stock"): pass
+class PortfolioDownloader(AlpacaPortfolioDownloader, Carryover, Producer, signature="->contract"): pass
 class ContractDownloader(AlpacaContractDownloader, Carryover, Processor, signature="symbol->contract"): pass
 class OptionDownloader(AlpacaOptionDownloader, Carryover, Processor, signature="contract->option"): pass
-class OptionFilter(Filter, Carryover, Processor, query=Querys.Settlement, signature="option->option"): pass
-class OptionCalculator(OptionCalculator, Carryover, Processor, signature="stock,option->option"): pass
-class StockCalculator(StockCalculator, Carryover, Processor, signature="stock->stock"): pass
+class OptionCalculator(OptionCalculator, Carryover, Processor, signature="option->option"): pass
 class OptionFilter(Filter, Carryover, Processor, query=Querys.Settlement, signature="option->option"): pass
 class StrategyCalculator(StrategyCalculator, Carryover, Processor, signature="option->strategy"): pass
 class ValuationCalculator(ValuationCalculator, Carryover, Processor, signature="strategy->valuation"): pass
 class ValuationFilter(Filter, Carryover, Processor, query=Querys.Settlement, signature="valuation->valuation"): pass
-class AcquisitionCalculator(AcquisitionCalculator, Carryover, Processor, signature="stock,option,valuation->acquisition"): pass
-class DivestitureCalculator(DivestitureCalculator, Carryover, Processor, signature="stock,option,valuation->divestiture"): pass
-class ProspectCalculator(ProspectCalculator, Carryover, Processor, signature="->prospect"): pass
-class OrderUploader(AlpacaOrderUploader, Carryover, Consumer, signature="prospect->"): pass
-
-class PortfolioDownloader(AlpacaPortfolioDownloader, Carryover, Producer, signature="->symbol,contract"):
-    pass
+class AcquisitionCalculator(AcquisitionCalculator, Carryover, Processor, signature="valuation,option->acquisition"): pass
+class DivestitureCalculator(DivestitureCalculator, Carryover, Processor, signature="valuation,option->divestiture"): pass
+class ProspectCalculator(ProspectCalculator, Carryover, Processor, signature="acquisition->prospect"): pass
+class OrderCalculator(AlpacaOrderUploader, Carryover, Consumer, signature="prospect->"): pass
 
 
+class Transaction(ntuple("Transaction", "acquisition divestiture")): pass
 class Criterions(ntuple("Criterion", "security valuation")): pass
 class SecurityCriterion(Criterion, fields=["size"]):
     def execute(self, table): return self.size(table)
@@ -78,47 +74,48 @@ class ValuationCriterion(Criterion, fields=["apy", "npv"]):
     def npv(self, table): return table[("npv", Variables.Valuations.Scenario.MINIMUM)] >= self["npv"]
 
 
-def acquisition(*args, source, symbols, priority, liquidity, **kwargs):
+def acquisition(*args, source, symbols, priority, liquidity, criterions, **kwargs):
     symbol_dequeuer = SymbolDequeuer(name="SymbolDequeuer", feed=symbols)
-    stock_downloader = StockDownloader(name="StockDownloader", source=source)
     contract_downloader = ContractDownloader(name="ContractDownloader", source=source)
     option_downloader = OptionDownloader(name="OptionDownloader", source=source)
     option_calculator = OptionCalculator(name="OptionCalculator", pricing=Variables.Markets.Pricing.AGGRESSIVE)
-    stock_calculator = StockCalculator(name="StockCalculator", pricing=Variables.Markets.Pricing.AGGRESSIVE)
+    option_filter = OptionFilter(name="OptionFilter", criterion=criterions.security)
     strategy_calculator = StrategyCalculator(name="StrategyCalculator", strategies=list(Strategies.Verticals))
     valuation_calculator = ValuationCalculator(name="ValuationCalculator", valuation=Variables.Valuations.Valuation.ARBITRAGE)
+    valuation_filter = ValuationFilter(name="ValuationFilter", criterion=criterions.valuation)
     acquisition_calculator = AcquisitionCalculator(name="AcquisitionCalculator", liquidity=liquidity, priority=priority)
-    acquisition_pipeline = symbol_dequeuer + stock_downloader + contract_downloader + option_downloader + option_calculator + stock_calculator
-    acquisition_pipeline = acquisition_pipeline + strategy_calculator + valuation_calculator + acquisition_calculator
+    prospect_calculator = ProspectCalculator(name="ProspectCalculator")
+    order_uploader = OrderCalculator(name="OrderUploader", source=source)
+    acquisition_pipeline = symbol_dequeuer + contract_downloader + option_downloader + option_calculator + option_filter + strategy_calculator
+    acquisition_pipeline = acquisition_pipeline + valuation_calculator + valuation_filter + acquisition_calculator + prospect_calculator + order_uploader
     return acquisition_pipeline
 
 
-def divestiture(*args, source, priority, liquidity, **kwargs):
+def divestiture(*args, source, priority, liquidity, criterions, **kwargs):
     portfolio_downloader = PortfolioDownloader(name="PortfolioDownloader", source=source)
-    stock_downloader = StockDownloader(name="StockDownloader", source=source)
     option_downloader = OptionDownloader(name="OptionDownloader", source=source)
     option_calculator = OptionCalculator(name="OptionCalculator", pricing=Variables.Markets.Pricing.AGGRESSIVE)
-    stock_calculator = StockCalculator(name="StockCalculator", pricing=Variables.Markets.Pricing.AGGRESSIVE)
+    option_filter = OptionFilter(name="OptionFilter", criterion=criterions.security)
     strategy_calculator = StrategyCalculator(name="StrategyCalculator", strategies=list(Strategies.Verticals))
     valuation_calculator = ValuationCalculator(name="ValuationCalculator", valuation=Variables.Valuations.Valuation.ARBITRAGE)
+    valuation_filter = ValuationFilter(name="ValuationFilter", criterion=criterions.valuation)
     divestiture_calculator = DivestitureCalculator(name="DivestitureCalculator", liquidity=liquidity, priority=priority)
-    divestiture_pipeline = portfolio_downloader + stock_downloader + option_downloader + option_calculator + stock_calculator
+    divestiture_pipeline = portfolio_downloader + option_downloader + option_calculator
     divestiture_pipeline = divestiture_pipeline + strategy_calculator + valuation_calculator + divestiture_calculator
     return divestiture_pipeline
 
 
-def main(*args, api, symbols=[], expires=[], criterion={}, parameters={}, **kwargs):
+def main(*args, api, symbols=[], expires=[], criterions, parameters={}, **kwargs):
     symbols = Queue.FIFO(contents=symbols, capacity=None, timeout=None)
     priority = lambda series: series[("apy", Variables.Valuations.Scenario.MINIMUM)]
-    liquidity = lambda series: series[("size", "") if isinstance(series.index, pd.MultiIndex) else "size"] * 0.5
-    criterions = Criterions(SecurityCriterion(**criterion), ValuationCriterion(**criterion))
+    liquidity = lambda series: round(series[("size", "") if isinstance(series.index, pd.MultiIndex) else "size"] * 0.25)
+    arguments = dict(symbols=symbols, expires=expires, priority=priority, liquidity=liquidity)
     parameters = dict(api=api, expires=expires) | dict(parameters)
 
     with WebReader(name="PaperTradeReader", delay=2) as source:
-        arguments = dict(symbols=symbols, expires=expires, priority=priority, liquidity=liquidity, criterions=criterions)
-        acquisitions = acquisition(*args, source=source, **arguments, **kwargs)
+        acquisitions = acquisition(*args, source=source, criterions=criterions.acquisition, **arguments, **kwargs)
         acquisitions = RoutineThread(acquisitions, name="AcquisitionThread").setup(**parameters)
-        divestitures = divestiture(*args, source=source, **arguments, **kwargs)
+        divestitures = divestiture(*args, source=source, criterions=criterions.divestiture, **arguments, **kwargs)
         divestitures = RepeatingThread(divestitures, name="DivestitureThread", wait=60).setup(**parameters)
         acquisitions.start()
         divestitures.start()
@@ -138,9 +135,11 @@ if __name__ == "__main__":
         sysExpires = DateRange([(Datetime.today() + Timedelta(days=1)).date(), (Datetime.today() + Timedelta(weeks=52)).date()])
     with open(API, "r") as apifile:
         sysAPI = WebAuthorizerAPI(*json.loads(apifile.read())["alpaca"])
-    sysCriterion = dict(apy=2.50, npv=50, size=10)
-    sysParameters = dict(discount=0.00, fees=0.00, term=Variables.Markets.Terms.MARKET, tenure=Variables.Markets.Tenure.FILLKILL, date=Datetime.now().date())
-    main(api=sysAPI, symbols=sysSymbols, expires=sysExpires, criterion=sysCriterion, parameters=sysParameters)
+    sysAcquisitions = Criterions(SecurityCriterion(size=10), ValuationCriterion(apy=10.00, npv=100))
+    sysDivestitures = Criterions(SecurityCriterion(size=10), ValuationCriterion(apy=0.00, npv=0))
+    sysCriterions = Transaction(sysAcquisitions, sysDivestitures)
+    sysParameters = dict(discount=0.00, fees=0.00, term=Variables.Markets.Term.MARKET, tenure=Variables.Markets.Tenure.FILLKILL, date=Datetime.now().date())
+    main(api=sysAPI, symbols=sysSymbols, expires=sysExpires, criterions=sysCriterions, parameters=sysParameters)
 
 
 
