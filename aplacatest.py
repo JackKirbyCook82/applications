ef3306b928bf1bf6eae353c7d53c6a1caf5953f3
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-11-12 05:12:11 -0600

    Updated 1/10/2024

--- aplacatest.py
+++ aplacatest.py
@@ -53,21 +53,61 @@ __license__ = "MIT License"
 
 Website = Enum("WebSite", "ALPACA ETRADE")
 class SymbolDequeuer(Dequeuer, Carryover, Producer, signature="->symbols"): pass
-class StockDownloader(AlpacaStockDownloader, Carryover, Processor, signature="symbols->stocks"): pass
-class ContractDownloader(AlpacaContractDownloader, Carryover, Processor, signature="symbols->contracts"): pass
-class BarDownloader(AlpacaBarsDownloader, Carryover, Processor, signature="symbols->bars"): pass
-class OptionDownloader(AlpacaOptionDownloader, Carryover, Processor, signature="contracts->options"): pass
-class TechnicalCalculator(TechnicalCalculator, Carryover, Processor, signature="bars->technicals"): pass
-class StockPricing(PricingCalculator, Carryover, Processor, query=Querys.Symbol, signature="stocks->stocks"): pass
-class OptionPricing(PricingCalculator, Carryover, Processor, query=Querys.Settlement, signature="options->options"): pass
-class AppraisalCalculator(AppraisalCalculator, Carryover, Processor, signature="options,technicals->options"): pass
-class SecurityCalculator(SecurityCalculator, Carryover, Processor, signature="stocks,options->securities"): pass
-class SecurityFilter(Filter, Carryover, Processor, query=Querys.Settlement, signature="securities->securities"): pass
-class StrategyCalculator(StrategyCalculator, Carryover, Processor, signature="securities->strategies"): pass
-class ValuationCalculator(ValuationCalculator, Carryover, Processor, signature="strategies->valuations"): pass
-class ValuationFilter(Filter, Carryover, Processor, query=Querys.Settlement, signature="valuations->valuations"): pass
-class ProspectCalculator(ProspectCalculator, Carryover, Processor, signature="securities,valuations->prospects"): pass
-class OrderUploader(AlpacaOrderUploader, Carryover, Consumer, signature="prospects->"): pass
+class StockDownloader(AlpacaStockDownloader, Carryover, Processor, signature="(symbols)->stocks"): pass
+class ContractDownloader(AlpacaContractDownloader, Carryover, Processor, signature="(symbols)->contracts"): pass
+class BarDownloader(AlpacaBarsDownloader, Carryover, Processor, signature="(symbols)->bars"): pass
+class OptionDownloader(AlpacaOptionDownloader, Carryover, Processor, signature="(contracts)->options"): pass
+class TechnicalCalculator(TechnicalCalculator, Carryover, Processor, signature="(bars)->technicals"): pass
+class StockPricing(PricingCalculator, Carryover, Processor, query=Querys.Symbol, signature="(stocks)->stocks"): pass
+class OptionPricing(PricingCalculator, Carryover, Processor, query=Querys.Settlement, signature="(options)->options"): pass
+class AppraisalCalculator(AppraisalCalculator, Carryover, Processor, signature="(options),{technicals}->options"): pass
+class SecurityCalculator(SecurityCalculator, Carryover, Processor, signature="(stocks,options)->securities"): pass
+class SecurityFilter(Filter, Carryover, Processor, query=Querys.Settlement, signature="(securities)->securities"): pass
+class SecurityImpliedCalculator(ImpliedCalculator, Carryover, Processor, signature="(securities)->securities"): pass
+class StrategyCalculator(StrategyCalculator, Carryover, Processor, signature="(securities)->strategies"): pass
+class ValuationCalculator(ValuationCalculator, Carryover, Processor, signature="(strategies)->valuations"): pass
+class ValuationFilter(Filter, Carryover, Processor, query=Querys.Settlement, signature="(valuations)->valuations"): pass
+class ProspectCalculator(ProspectCalculator, Carryover, Processor, signature="(valuations)->prospects"): pass
+class OrderUploader(AlpacaOrderUploader, Carryover, Consumer, signature="(prospects)->"): pass
+
+class OptionImpliedCalculator(ImpliedCalculator, Carryover, Processor, signature="(options)->options"): pass
+class OptionCurveCalculator(Carryover, Consumer, signature="(options)->"):
+    def execute(self, options, /, **kwargs):
+        print(options)
+        raise Exception()
+
+
+def download(*args, feed, source, webapi, **kwargs):
+    symbols_dequeuer = SymbolDequeuer(name="SymbolDequeuer", feed=feed)
+    stocks_downloader = StockDownloader(name="StockDownloader", source=source, webapi=webapi[Website.ALPACA])
+    contract_downloader = ContractDownloader(name="ContractDownloader", source=source, webapi=webapi[Website.ALPACA])
+    bar_downloader = BarDownloader(name="BarDownloader", source=source, webapi=webapi[Website.ALPACA])
+    options_downloader = OptionDownloader(name="OptionDownloader", source=source, webapi=webapi[Website.ALPACA])
+    return symbols_dequeuer + stocks_downloader + contract_downloader + bar_downloader + options_downloader
+
+def appraise(producer, *args, technicals, appraisals, pricing, **kwargs):
+    technical_calculator = TechnicalCalculator(name="TechnicalCalculator", technicals=technicals)
+    stock_pricing = StockPricing(name="StockPricing", pricing=pricing.stock)
+    option_pricing = OptionPricing(name="OptionPricing", pricing=pricing.option)
+    appraisal_calculator = AppraisalCalculator(name="AppraisalCalculator", appraisals=appraisals)
+    return producer + technical_calculator + stock_pricing + option_pricing + appraisal_calculator
+
+def security(producer, *args, criteria, **kwargs):
+    security_calculator = SecurityCalculator(name="SecurityCalculator")
+    security_filter = SecurityFilter(name="SecurityFilter", criteria=criteria)
+    implied_calculator = SecurityImpliedCalculator(name="ImpliedCalculator")
+    return producer  + security_calculator + security_filter + implied_calculator
+
+def valuation(producer, *args, strategies, criteria, **kwargs):
+    strategy_calculator = StrategyCalculator(name="StrategyCalculator", strategies=strategies)
+    valuation_calculator = ValuationCalculator(name="ValuationCalculator")
+    valuation_filter = ValuationFilter(name="ValuationFilter", criteria=criteria)
+    return producer + strategy_calculator + valuation_calculator + valuation_filter
+
+def prospect(producer, *args, source, webapi, priority, liquidity, **kwargs):
+    prospects_calculator = ProspectCalculator(name="ProspectCalculator", priority=priority, liquidity=liquidity)
+    order_uploader = OrderUploader(name="OrderUploader", source=source, webapi=webapi[Website.ALPACA])
+    return producer + prospects_calculator + order_uploader
 
 
 def main(*args, symbols=[], webapi={}, delayers={}, parameters={}, **kwargs):
@@ -80,33 +120,32 @@ def main(*args, symbols=[], webapi={}, delayers={}, parameters={}, **kwargs):
     value_criteria = lambda table: table["npv"] >= + 100
     cost_criteria = lambda table: table["spot"] >= - 500
     valuation_criteria = lambda table: value_criteria(table) & cost_criteria(table)
+    pricing = ntuple("Pricing", "stock option")(stock_pricing, option_pricing)
     appraisals = [Concepts.Appraisal.BLACKSCHOLES]
     technicals = [Concepts.Technical.STATISTIC]
     strategies = list(Strategies)
 
+    # with WebReader(delayer=delayers[Website.ALPACA]) as alpaca_source:
+    #     download_pipeline = download(feed=symbol_feed, source=alpaca_source, webapi=webapi)
+    #     appraise_pipeline = appraise(download_pipeline, technicals=technicals, appraisals=appraisals, pricing=pricing)
+    #     implied_calculator = OptionImpliedCalculator(name="ImpliedCalculator")
+    #     curve_calculator = OptionCurveCalculator(name="CurveCalculator")
+    #     curve_pipeline = appraise_pipeline + implied_calculator + curve_calculator
+    #     curve_thread = RoutineThread(curve_pipeline, name="AlpacaCurveThread").setup(**parameters)
+    #     curve_thread.start()
+    #     curve_thread.join()
+    # return
+
     with WebReader(delayer=delayers[Website.ALPACA]) as alpaca_source:
-        symbols_dequeuer = SymbolDequeuer(name="SymbolDequeuer", feed=symbol_feed)
-        stocks_downloader = StockDownloader(name="StockDownloader", source=alpaca_source, webapi=webapi[Website.ALPACA])
-        contract_downloader = ContractDownloader(name="ContractDownloader", source=alpaca_source, webapi=webapi[Website.ALPACA])
-        bar_downloader = BarDownloader(name="BarDownloader", source=alpaca_source, webapi=webapi[Website.ALPACA])
-        options_downloader = OptionDownloader(name="OptionDownloader", source=alpaca_source, webapi=webapi[Website.ALPACA])
-        technical_calculator = TechnicalCalculator(name="TechnicalCalculator", technicals=technicals)
-        stock_pricing = StockPricing(name="StockPricing", pricing=stock_pricing)
-        option_pricing = OptionPricing(name="OptionPricing", pricing=option_pricing)
-        appraisal_calculator = AppraisalCalculator(name="AppraisalCalculator", appraisals=appraisals)
-        security_calculator = SecurityCalculator(name="SecurityCalculator")
-        security_filter = SecurityFilter(name="SecurityFilter", criteria=security_criteria)
-        strategy_calculator = StrategyCalculator(name="StrategyCalculator", strategies=strategies)
-        valuation_calculator = ValuationCalculator(name="ValuationCalculator")
-        valuation_filter = ValuationFilter(name="ValuationFilter", criteria=valuation_criteria)
-        prospects_calculator = ProspectCalculator(name="ProspectCalculator", priority=prospect_priority, liquidity=prospect_liquidity)
-        order_uploader = OrderUploader(name="OrderUploader", source=alpaca_source, webapi=webapi[Website.ALPACA])
-        algotrade_pipeline = symbols_dequeuer + stocks_downloader + contract_downloader + bar_downloader + options_downloader
-        algotrade_pipeline = algotrade_pipeline + technical_calculator + stock_pricing + option_pricing + appraisal_calculator + security_calculator + security_filter
-        algotrade_pipeline = algotrade_pipeline + strategy_calculator + valuation_calculator + valuation_filter + prospects_calculator + order_uploader
-        thread = RoutineThread(algotrade_pipeline, name="AlpacaTradeThread").setup(**parameters)
-        thread.start()
-        thread.join()
+        download_pipeline = download(feed=symbol_feed, source=alpaca_source, webapi=webapi)
+        appraise_pipeline = appraise(download_pipeline, technicals=technicals, appraisals=appraisals, pricing=pricing)
+        security_pipeline = security(appraise_pipeline, criteria=security_criteria)
+        valuation_pipeline = valuation(security_pipeline, strategies=strategies, criteria=valuation_criteria)
+        prospect_pipeline = prospect(valuation_pipeline, source=alpaca_source, webapi=webapi, priority=prospect_priority, liquidity=prospect_liquidity)
+        prospect_thread = RoutineThread(prospect_pipeline, name="AlpacaProspectThread").setup(**parameters)
+        prospect_thread.start()
+        prospect_thread.join()
+    return
 
 
 if __name__ == "__main__":
