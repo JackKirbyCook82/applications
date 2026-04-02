243a313003d81a90dbcfa3771245ad5f99fe9904
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-04-02 01:19:58 -0500

    Updated 1/10/2024

--- trading.py
+++ trading.py
@@ -101,11 +101,9 @@ def main(*args, tickers, history, expires, strikes, interest, discount, fees, pe
             options = sanity_filter(options)
             options = viability_filter(options, spread=0.25, size=2)
             options = option_calculator(options, interest=interest)
-
-            print(options)
-
             options = greek_calculator(options=options, interest=interest)
 
+            options.to_csv(os.path.join(REPOSITORY, "option_greeks.txt"))
             print(options)
             raise Exception()
 
