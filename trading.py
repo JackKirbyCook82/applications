e3426a052d2429ce981f55a90a734ea6bd40c62f
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-04-20 05:19:32 -0500

    Updated 1/10/2024

--- trading.py
+++ trading.py
@@ -92,7 +92,7 @@ def main(*args, tickers, history, expires, strikes, period, interest, dividends,
         valuation_calculator = ValuationCalculator(name="ValuationCalculator")
         greek_calculator = GreekCalculator(name="GreekCalculator")
         surface_calculator = SurfaceCalculator(name="SurfaceCalculator")
-        surface_creator = SurfaceCreator(name="SurfaceCreator", samplesize=5, gridsize=100, curvetype="natural", degree=(3, 3))
+        surface_creator = SurfaceCreator(name="SurfaceCreator", samplesize=5, gridsize=100, curvetype="natural", degree=(2, 3), smoothing=1e-2)
         surface_plotter = SurfacePlotter(name="SurfacePlotter", figsize=(10, 10), gridsize=100)
 
         while bool(symbols):
@@ -117,8 +117,8 @@ def main(*args, tickers, history, expires, strikes, period, interest, dividends,
             options = greek_calculator(options, interest=interest, dividends=dividends)
             options = surface_calculator(options)
             surface = surface_creator(options)
-            surface_plotter(options, surface)
 
+            surface_plotter(options, surface)
             print(options)
             raise Exception()
 
