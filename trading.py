c87e4d6cb823663f6d7f491e89fdba3c1358bc6f
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-06-21 02:38:45 -0500

    Updated 1/10/2024

--- trading.py
+++ trading.py
@@ -32,7 +32,7 @@ from alpaca.market import AlpacaStockDownloader, AlpacaContractDownloader, Alpac
 from alpaca.history import AlpacaBarsDownloader
 from alpaca.orders import AlpacaSpreadUploader
 from stocks.technicals import TechnicalCalculator
-from options.markets import MarketCalculator, SanityFilter, ViabilityFilter
+from options.markets import MarketCalculator, SanityFilter, ViabilityCalculator
 from options.volatility import VolatilityCalculator
 from options.valuations import ValuationCalculator
 from options.forwards import ForwardCalculator
@@ -101,7 +101,7 @@ def main(*args, tickers, history, expires, strikes, period, interest, dividends,
         technical_calculator = TechnicalCalculator(name="TechnicalCalculator", technicals=technicals)
         sanity_filter = SanityFilter(name="SanityFilter")
         market_calculator = MarketCalculator(name="MarketCalculator")
-        viability_filter = ViabilityFilter(name="ViabilityFilter", size=5, money=0.20, tight=0.20)
+        viability_calculator = ViabilityCalculator(name="ViabilityCalculator", size=5, money=0.20, tight=0.20)
         forward_calculator = ForwardCalculator(name="ForwardCalculator", samplesize=5, tight=0.20)
         volatility_calculator = VolatilityCalculator(name="VolatilityCalculator", low=1e-4, high=5.0, tol=1e-10, iters=100)
         valuation_calculator = ValuationCalculator(name="ValuationCalculator")
@@ -130,11 +130,11 @@ def main(*args, tickers, history, expires, strikes, period, interest, dividends,
             options["spot"] = stock["median"]
             options = sanity_filter(options)
             options = market_calculator(options)
+            options = viability_calculator(options)
 
             print(options)
             raise Exception()
 
-            options = viability_filter(options)
             options = forward_calculator(options, interest=interest, dividends=dividends)
             options = valuation_calculator(options, interest=interest, dividends=dividends)
             options = volatility_calculator(options, interest=interest, dividends=dividends)
