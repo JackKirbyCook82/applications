4b9ba89954344aebcc085399afe12575eb1d4151
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-07-19 01:17:12 -0500

    Updated 1/10/2024

--- acquisitions.py
+++ acquisitions.py
@@ -22,13 +22,14 @@ RESOURCES = ROOT / "resources"
 AUTHENTICATORS = RESOURCES / "authenticators.txt"
 ACCOUNTS = RESOURCES / "accounts.txt"
 
-from solutions.trading import OptionDownloading, OptionFiltering, OptionCalculating, OptionLocalizing
+from solutions.options import OptionDownloading, OptionFiltering, OptionMarketing, OptionForecasting
 from alpaca.market import AlpacaStockDownloader, AlpacaContractDownloader, AlpacaOptionDownloader
 from alpaca.portfolio import AlpacaPortfolioDownloader
 from options import OptionCalculator, SanityFilter, ViabilityFilter
 from options.localizing import PartitionCalculator, ProximityCalculator, Localizing
 from options.variances import VarianceCalculator, VarianceScreener, VarianceStandardizer
 from options.volatility import VolatilityCalculator
+from options.valuations import ValuationCalculator
 from options.forwards import ForwardCalculator
 from options.greeks import GreekCalculator
 from finance.brokers import Authenticator, Brokerage
@@ -45,7 +46,7 @@ __copyright__ = "Copyright 2026, Jack Kirby Cook"
 __license__ = "MIT License"
 
 
-def main(*args, tickers, expires, history, strikes, term, tenure, period, interest, dividends, **kwargs):
+def main(*args, tickers, expires, strikes, term, tenure, interest, dividends, **kwargs):
     localizing = Localizing.create(radius=(0.05, 0.12, 0.01), window=(1, 3, 1), coverage=(3, 10), limit=45/365)
     brokerage = Brokerage(Website.ALPACA, False)
     authenticator = Authenticator.load(AUTHENTICATORS)[brokerage]
@@ -53,35 +54,35 @@ def main(*args, tickers, expires, history, strikes, term, tenure, period, intere
 
     with WebReader(delay=1) as source:
         portfolio_downloader = AlpacaPortfolioDownloader(name="PortfolioDownloader", source=source, authenticator=authenticator)
-        stocks = AlpacaStockDownloader(name="StockDownloader", source=source, authenticator=authenticator)
-        contracts = AlpacaContractDownloader(name="ContractDownloader", source=source, authenticator=authenticator)
-        options = AlpacaOptionDownloader(name="OptionDownloader", source=source, authenticator=authenticator)
-        downloading = OptionDownloading(stocks=stocks, contracts=contracts, options=options)
+        stock_downloader = AlpacaStockDownloader(name="StockDownloader", source=source, authenticator=authenticator)
+        contract_downloader = AlpacaContractDownloader(name="ContractDownloader", source=source, authenticator=authenticator)
+        option_downloader = AlpacaOptionDownloader(name="OptionDownloader", source=source, authenticator=authenticator)
+        sanity_filter = SanityFilter(name="SanityFilter", size=5)
+        option_calculator = OptionCalculator(name="OptionCalculator")
+        viability_filter = ViabilityFilter(name="ViabilityFilter", active=0.30, money=0.15, tight=0.15)
+        volatility_calculator = VolatilityCalculator(name="VolatilityCalculator", low=1e-4, high=5.0, tol=1e-10, iters=100)
+        valuation_calculator = ValuationCalculator(name="ValuationCalculator")
+        greek_calculator = GreekCalculator(name="GreekCalculator")
+        forward_calculator = ForwardCalculator(name="ForwardCalculator", samplesize=5, tightness=0.15)
+        variance_calculator = VarianceCalculator(name="VarianceCalculator")
+        variance_screener = VarianceScreener(name="VarianceScreener", neighbors=25, quantile=0.95, multiple=2.5)
+        variance_standardizer = VarianceStandardizer(name="VarianceStandardizer", neighbors=25)
+        surface_creator = SurfaceCreator(name="SurfaceCreator", columns="tau|mae|tiv", quantity=35, gridsize=100, samplesize=5)
+        partition_calculator = PartitionCalculator(name="PartitionCalculator", localizing=localizing, samples=35, overlap=0.80)
+        proximity_calculator = ProximityCalculator(name="ProximityCalculator", localizing=localizing, samples=35, overlap=0.80)
 
-        sanity = SanityFilter(name="SanityFilter", size=5)
-        options = OptionCalculator(name="OptionCalculator")
-        viability = ViabilityFilter(name="ViabilityFilter", active=0.30, money=0.15, tight=0.15)
-        filtering = OptionFiltering(sanity=sanity, options=options, viability=viability)
-
-        forward = ForwardCalculator(name="ForwardCalculator", samplesize=5, tightness=0.15)
-        volatility = VolatilityCalculator(name="VolatilityCalculator", low=1e-4, high=5.0, tol=1e-10, iters=100)
-        variance = VarianceCalculator(name="VarianceCalculator")
-        screener = VarianceScreener(name="VarianceScreener", neighbors=25, quantile=0.95, multiple=2.5)
-        greeks = GreekCalculator(name="GreekCalculator")
-        computing = OptionCalculating(forward=forward, volatility=volatility, variance=variance, screener=screener, greeks=greeks)
-
-        surface = SurfaceCreator(name="SurfaceCreator", columns="tau|mae|tiv", quantity=35, gridsize=100, samplesize=5)
-        partitions = PartitionCalculator(name="PartitionCalculator", localizing=localizing, samples=35, overlap=0.80)
-        proximity = ProximityCalculator(name="ProximityCalculator", localizing=localizing, samples=35, overlap=0.80)
-        localizing = OptionLocalizing(surface=surface, partitions=partitions, proximity=proximity)
-
-        standardize = VarianceStandardizer(name="VarianceStandardizer", neighbors=25)
+        downloading = OptionDownloading(stocks=stock_downloader, contracts=contract_downloader, options=option_downloader)
+        filtering = OptionFiltering(sanity=sanity_filter, options=option_calculator, viability=viability_filter)
+        marketing = OptionMarketing(volatility=volatility_calculator, greeks=greek_calculator, forward=forward_calculator, variance=variance_calculator, screener=variance_screener)
+        forecasting = OptionForecasting(surface=surface_creator, standardize=variance_standardizer, valuation=valuation_calculator)
 
         portfolio = portfolio_downloader()
         for symbol in symbols:
             options = downloading(symbol, expires=expires, strikes=strikes)
             options = filtering(options)
-            options = computing(options, interest=interest, dividends=dividends)
+            options = marketing(options, interest=interest, dividends=dividends)
+            for localized in partition_calculator(options):
+                localized = forecasting(localized, interest=interest, dividends=dividends)
 
 
 if __name__ == "__main__":
@@ -93,10 +94,9 @@ if __name__ == "__main__":
     arguments, parameters = list(), dict()
     parameters["tickers"] = ["SPY", "QQQ", "TSLA", "NVDA"]
     parameters["expires"] = DateRange.create([(Datetime.today() + Timedelta(days=1)).date(), (Datetime.today() + Timedelta(weeks=52*1/12)).date()])
-    parameters["history"] = DateRange.create([(Datetime.today() - Timedelta(weeks=52*2)).date(), (Datetime.today() - Timedelta(days=1)).date()])
     parameters["strikes"] = NumRange.create([0.95, 1.05])
     parameters.update({"term": Terms.LIMIT, "tenure": Tenure.DAY})
-    parameters.update({"period": 252, "interest": np.log10(1 + 0.05), "dividends": np.log10(1 + 0.00)})
+    parameters.update({"interest": np.log10(1 + 0.05), "dividends": np.log10(1 + 0.00)})
     main(*arguments, **parameters)
 
 
