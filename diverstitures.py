7d499827efcd7eb27186c6213e9434c8dbbf708b
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-07-20 04:57:08 -0500

    Updated 1/10/2024

--- diverstitures.py
+++ diverstitures.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jul 6 2026
-@name:   Trading Acquisitions Application
+@name:   Trading Divestitures Application
 @author: Jack Kirby Cook
 
 """
@@ -12,8 +12,6 @@ import warnings
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from datetime import datetime as Datetime
-from datetime import timedelta as Timedelta
 
 ROOT = Path(__file__).resolve().parent.parent
 if str(ROOT) not in sys.path: sys.path.append(str(ROOT))
@@ -24,8 +22,9 @@ ACCOUNTS = RESOURCES / "accounts.txt"
 
 from solutions.options import OptionDownloading, OptionFiltering, OptionMarketing, OptionForecasting
 from alpaca.market import AlpacaStockDownloader, AlpacaContractDownloader, AlpacaOptionDownloader
+from alpaca.portfolio import AlpacaPortfolioDownloader
 from options import OptionCalculator, SanityFilter, ViabilityFilter
-from options.localizing import PartitionCalculator, Localizing
+from options.localizing import ProximityCalculator, Localizing
 from options.variances import VarianceCalculator, VarianceScreener, VarianceStandardizer
 from options.volatility import VolatilityCalculator
 from options.valuations import ValuationCalculator
@@ -35,7 +34,6 @@ from finance.brokers import Authenticator, Brokerage
 from finance.enumerations import Website, Terms, Tenure
 from finance.querys import Symbol
 from webscraping.webreaders import WebReader
-from support.custom import NumRange, DateRange
 from support.surface import SurfaceCreator
 
 __version__ = "1.0.0"
@@ -45,13 +43,13 @@ __copyright__ = "Copyright 2026, Jack Kirby Cook"
 __license__ = "MIT License"
 
 
-def main(*args, tickers, expires, strikes, term, tenure, interest, dividends, **kwargs):
+def main(*args, term, tenure, interest, dividends, **kwargs):
     localizing = Localizing.create(radius=(0.05, 0.12, 0.01), window=(1, 3, 1), coverage=(3, 10), limit=45/365)
     brokerage = Brokerage(Website.ALPACA, False)
     authenticator = Authenticator.load(AUTHENTICATORS)[brokerage]
-    symbols = list(map(Symbol, tickers))
 
     with WebReader(delay=1) as source:
+        portfolio_downloader = AlpacaPortfolioDownloader(name="PortfolioDownloader", source=source, authenticator=authenticator)
         stock_downloader = AlpacaStockDownloader(name="StockDownloader", source=source, authenticator=authenticator)
         contract_downloader = AlpacaContractDownloader(name="ContractDownloader", source=source, authenticator=authenticator)
         option_downloader = AlpacaOptionDownloader(name="OptionDownloader", source=source, authenticator=authenticator)
@@ -66,19 +64,21 @@ def main(*args, tickers, expires, strikes, term, tenure, interest, dividends, **
         variance_screener = VarianceScreener(name="VarianceScreener", neighbors=25, quantile=0.95, multiple=2.5)
         variance_standardizer = VarianceStandardizer(name="VarianceStandardizer", neighbors=25)
         surface_creator = SurfaceCreator(name="SurfaceCreator", columns="tau|mae|tiv", quantity=35, gridsize=100, samplesize=5)
-        partition_calculator = PartitionCalculator(name="PartitionCalculator", localizing=localizing, samples=35, overlap=0.80)
+        proximity_calculator = ProximityCalculator(name="ProximityCalculator", localizing=localizing, samples=35, overlap=0.80)
 
         downloading = OptionDownloading(stocks=stock_downloader, contracts=contract_downloader, options=option_downloader)
         filtering = OptionFiltering(sanity=sanity_filter, options=option_calculator, viability=viability_filter)
         marketing = OptionMarketing(volatility=volatility_calculator, greeks=greek_calculator, forward=forward_calculator, variance=variance_calculator, screener=variance_screener)
         forecasting = OptionForecasting(surface=surface_creator, standardize=variance_standardizer, valuation=valuation_calculator)
 
-        for symbol in symbols:
-            options = downloading(symbol, expires=expires, strikes=strikes)
+        portfolio = portfolio_downloader()
+        for ticker, holdings in portfolio.groupby("ticker"):
+            symbol = Symbol(ticker)
+            options = downloading(symbol, expires=, strikes=)
             options = filtering(options)
             options = marketing(options, interest=interest, dividends=dividends)
-            for localized in partition_calculator(options):
-                localized = forecasting(localized, interest=interest, dividends=dividends)
+
+#            localized = forecasting(localized, interest=interest, dividends=dividends)
 
 
 if __name__ == "__main__":
@@ -88,9 +88,8 @@ if __name__ == "__main__":
     pd.set_option("display.max_rows", 50)
     pd.set_option("display.width", 250)
     arguments, parameters = list(), dict()
-    parameters["tickers"] = ["SPY", "QQQ", "TSLA", "NVDA"]
-    parameters["expires"] = DateRange.create([(Datetime.today() + Timedelta(days=1)).date(), (Datetime.today() + Timedelta(weeks=52*1/12)).date()])
-    parameters["strikes"] = NumRange.create([0.95, 1.05])
+    parameters["expires"] =
+    parameters["strikes"] =
     parameters.update({"term": Terms.LIMIT, "tenure": Tenure.DAY})
     parameters.update({"interest": np.log10(1 + 0.05), "dividends": np.log10(1 + 0.00)})
     main(*arguments, **parameters)
